8b6a42d7aec39359339bebfe8b17d7120018b621
Iman <imangholamiimi@gmail.com>
2024-09-14 19:31:40 +0330

    :sparkles: | ADD | func >> send_mail_by_day

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 import smtplib
-from decouple import config
+from datetime import datetime
 from random import choice
+
+from decouple import config
 
 sender_adr = config('EMAIL')
 sender_app_password = config('PASSWORD')
 recipient_adr = config('RECIPIENT')
 
+
 # make connection
-def send_mail():
+def send_mail(message):
     with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
         connection.starttls()  # to send messages encoded into net
         connection.login(user=sender_adr, password=sender_app_password)
         connection.sendmail(
             from_addr=sender_adr,
             to_addrs=recipient_adr,
-            msg="Subject:Iman_Gholami\n\nthat was changes to subject and here is body."
-            )
+            msg=f"Subject:Happy Birthday\n\n{message}"
+        )
 
 
 with open(file="./quotes.txt", mode="r") as file:
     data = file.readlines()
     quotes_list = [quote.strip() for quote in data]
 
-print(choice(quotes_list))
+
+def send_mail_by_day():
+    day = 5
+    if datetime.now().weekday() == day:
+        MESSAGE = choice(quotes_list)
+        send_mail(message=MESSAGE)
+        print("Email Sent.")
+    else:
+        print("Error!")
+
+
+send_mail_by_day()
