330932f030ce49cf61ce0f8f1419f1bce5406774
Iman <imangholamiimi@gmail.com>
2024-09-14 19:41:22 +0330

    :zap: | UPDATE | changes message

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ recipient_adr = config('RECIPIENT')
 
 # make connection
 def send_mail(message):
-    with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
+    with smtplib.SMTP(host='smtp.gmail.com', port=587) as connection:
         connection.starttls()  # to send messages encoded into net
         connection.login(user=sender_adr, password=sender_app_password)
         connection.sendmail(
             from_addr=sender_adr,
             to_addrs=recipient_adr,
-            msg=f"Subject:Happy Birthday\n\n{message}"
+            msg=f"Subject:Happy Birthday🎉\n\n{message}\nHappy Birthday to you 😊"
         )
 
 
@@ -27,6 +27,7 @@ with open(file="./quotes.txt", mode="r") as file:
 
 
 def send_mail_by_day():
+    """checks date and choice random message to send by calling send mail"""
     day = 5
     if datetime.now().weekday() == day:
         MESSAGE = choice(quotes_list)
