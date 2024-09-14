19801eb3fce72c261048b7eb765a4c17c4a78468
Iman <imangholamiimi@gmail.com>
2024-09-14 20:59:01 +0330

    :sparkles: | ADD | Func >> send_mail_by_mail: read data from csv, check day and month of data, send mail to data matches

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from random import choice
 
 from decouple import config
+from pandas import read_csv
 
 sender_adr = config('EMAIL')
 sender_app_password = config('PASSWORD')
@@ -26,15 +27,26 @@ with open(file="./quotes.txt", mode="r") as file:
     quotes_list = [quote.strip() for quote in data]
 
 
-def send_mail_by_day():
+def send_mail_by_day(date_list):
     """checks date and choice random message to send by calling send mail"""
-    day = 5
-    if datetime.now().weekday() == day:
-        MESSAGE = choice(quotes_list)
-        send_mail(message=MESSAGE)
-        print("Email Sent.")
-    else:
-        print("Error!")
+    for date in date_list:
+        if datetime.now().weekday() == date[1] and datetime.now().month == date[0]:
+            file = choice([1, 2, 3])
+            MESSAGE = choice(quotes_list)
+            with open(file=f"letter_templates/letter_{file}.txt", mode="r") as file:
+                text = file.readlines()
+                new_text = [line.replace('[NAME]', birthdays_df.name).replace("[message]", MESSAGE) for line in text if
+                 '[NAME]' in line or '[message]' in line]
+            with open(file=f"{birthdays_df.name}_in_{datetime.now().year}.txt", mode="w") as output_file:
+                output_file.writelines(new_text)
+                print(output_file)
+            # send_mail(message=output_file)
+            print("Email Sent Successfully.")
+        else:
+            print("Error!\nunable to send mail.")
 
 
-send_mail_by_day()
+birthdays_df = read_csv("birthdays.csv")
+dates = [(row.month, row.day) for (index, row) in birthdays_df.iterrows()]
+
+send_mail_by_day(date_list=dates)
