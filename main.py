ac83440314f34ebdc41c78bf756b727780777470
Iman <imangholamiimi@gmail.com>
2024-09-15 12:32:03 +0330

    :zap: | UPDATE | Func >> send_mail_by_day: get index of data and related name

--- main.py
+++ main.py
@@ -30,14 +30,17 @@ with open(file="./quotes.txt", mode="r") as file:
 def send_mail_by_day(date_list):
     """checks date and choice random message to send by calling send mail"""
     for date in date_list:
-        if datetime.now().weekday() == date[1] and datetime.now().month == date[0]:
+        if (datetime.now().weekday() == datetime(date[1], date[2], date[3]).weekday() and
+                datetime.now().month == datetime(date[1], date[2], date[3]).month):
+            index = date[0]
+            name = birthdays_df.iloc[index]['name']
             file = choice([1, 2, 3])
             MESSAGE = choice(quotes_list)
-            with open(file=f"letter_templates/letter_{file}.txt", mode="r") as file:
-                text = file.readlines()
-                new_text = [line.replace('[NAME]', birthdays_df.name).replace("[message]", MESSAGE) for line in text if
-                 '[NAME]' in line or '[message]' in line]
-            with open(file=f"{birthdays_df.name}_in_{datetime.now().year}.txt", mode="w") as output_file:
+            with open(file=f"letter_templates/letter_{file}.txt", mode="r") as letter_file:
+                text = letter_file.readlines()
+                new_text = [line.replace('[NAME]', name).replace("[message]", MESSAGE)
+                            for line in text if '[NAME]' in line or '[message]' in line or line]
+            with open(file=f"{name}_in_{datetime.now().year}.txt", mode="w") as output_file:
                 output_file.writelines(new_text)
                 print(output_file)
             # send_mail(message=output_file)
@@ -47,6 +50,6 @@ def send_mail_by_day(date_list):
 
 
 birthdays_df = read_csv("birthdays.csv")
-dates = [(row.month, row.day) for (index, row) in birthdays_df.iterrows()]
+dates = [(index, row.year, row.month, row.day) for (index, row) in birthdays_df.iterrows()]
 
 send_mail_by_day(date_list=dates)
