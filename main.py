22a9bac648580c412a3d808f919a4f24a4342edd
Iman <imangholamiimi@gmail.com>
2024-09-15 13:24:02 +0330

    :sparkles: | ADD | Module >> email.mime.text: to resolve unicode error | :zap: | Exception Handling: to catch OS Errors

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
 import smtplib
+from email.mime.text import MIMEText
+
 from datetime import datetime
 from random import choice
 
@@ -12,13 +15,19 @@ recipient_adr = config('RECIPIENT')
 
 # make connection
 def send_mail(message):
+    """Create the email message using MIMEText with UTF-8 encoding"""
+    msg = MIMEText(f"{message} 😊", 'plain', 'utf-8')
+    msg['Subject'] = "Happy Birthday🎉"
+    msg['From'] = sender_adr
+    msg['To'] = recipient_adr
+
     with smtplib.SMTP(host='smtp.gmail.com', port=587) as connection:
         connection.starttls()  # to send messages encoded into net
         connection.login(user=sender_adr, password=sender_app_password)
         connection.sendmail(
             from_addr=sender_adr,
             to_addrs=recipient_adr,
-            msg=f"Subject:Happy Birthday🎉\n\n{message}\nHappy Birthday to you 😊"
+            msg=msg.as_string()
         )
 
 
@@ -34,22 +43,31 @@ def send_mail_by_day(date_list):
                 datetime.now().month == datetime(date[1], date[2], date[3]).month):
             index = date[0]
             name = birthdays_df.iloc[index]['name']
-            file = choice([1, 2, 3])
-            MESSAGE = choice(quotes_list)
-            with open(file=f"letter_templates/letter_{file}.txt", mode="r") as letter_file:
+            number = choice([1, 2, 3])
+            message = choice(quotes_list)
+            with open(file=f"letter_templates/letter_{number}.txt", mode="r") as letter_file:
                 text = letter_file.readlines()
-                new_text = [line.replace('[NAME]', name).replace("[message]", MESSAGE)
-                            for line in text if '[NAME]' in line or '[message]' in line or line]
-            with open(file=f"{name}_in_{datetime.now().year}.txt", mode="w") as output_file:
-                output_file.writelines(new_text)
-                print(output_file)
-            # send_mail(message=output_file)
-            print("Email Sent Successfully.")
-        else:
-            print("Error!\nunable to send mail.")
+                new_text = [line.replace('[NAME]', name).replace("[message]", message) for line in text]
+            try:
+                os.mkdir("./sent_mails")
+            except FileExistsError:
+                pass
+            except OSError as e:
+                print(f"An error occurred while creating the directory: {e}")
+            finally:
+                with open(file=f"sent_mails/{name}_in_{datetime.now().year}.txt", mode="w") as output_file:
+                    output_file.writelines(new_text)
+            send_mail(message="".join(new_text))
+            print(f"Email Sent to {name} Successfully.")
 
 
 birthdays_df = read_csv("birthdays.csv")
 dates = [(index, row.year, row.month, row.day) for (index, row) in birthdays_df.iterrows()]
 
-send_mail_by_day(date_list=dates)
+
+def main():
+    send_mail_by_day(date_list=dates)
+
+
+if __name__ == '__main__':
+    main()
